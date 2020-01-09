c55179573e231ba6e48b096f3424d2b44bf58ffa
Felipe Brandelli <felipe.brandelli@bgdigital.com.br>
2020-01-09 08:52:29 -0300

    Primeiro commit

---
+++ Employees/Funcionario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employees
+{
+    class Funcionario
+    {
+        public int id { get; private set; }
+        public string name { get; set; }
+        public double salary { get; private set; }
+
+        public Funcionario(int id, string name, double salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public void IncreaseSalary (double percentage)
+        {
+            this.salary = salary + salary * (percentage / 100);
+        }
+
+    }
+}
---
+++ Employees/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Employees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many employees will be registered? ");
+            int n = int.Parse(Console.ReadLine());
+            List<Funcionario> list = new List<Funcionario> { };
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Employee #" + i);
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine());
+                Console.WriteLine("");
+
+                Funcionario employee = new Funcionario(id, name, salary);
+                list.Add(employee);
+            }
+            Console.Write("Enter the employee id that will have salary increase: ");
+            int idIncrease = int.Parse(Console.ReadLine());
+            Funcionario employeeIncrease = list.Find(x => x.id == idIncrease);
+            if (employeeIncrease == null)
+            {
+                Console.WriteLine("This id does not exist!");
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine());
+                employeeIncrease.IncreaseSalary(percentage);
+                Console.WriteLine("");
+            }
+            Console.WriteLine("Update list of employees:");
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.id},{item.name},{item.salary}");
+            }
+        }
+    }
+}
